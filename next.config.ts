872875c408d9387eafbccdbf14e746b0c7194960
BlackLiBeautySalon <krasisalon553@gmail.com>
2026-05-14 21:03:08 +0200

    x

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  sassOptions: {
+    silenceDeprecations: ["legacy-js-api", "import"],
+  },
 };
 
 export default nextConfig;
